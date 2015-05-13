57bd3a785a1a6c0c22c78b4f712299174b4eaa60
chris bustamante <chrisbusta@hotmsil.com>
2015-05-12 20:18:30 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-function
+
+
+
+
+
+var number1 = document.querySelector("#num_one");
+var number2 = document.querySelector("#num_two");
+var answer = document.querySelector("#answer");
+var btn = document.querySelector("#calculateBtn");
+
+function add () {
+  var num1 = Number(number1.value);
+  var num2 = Number(number2.value);
+  var sum = num1 + num2;
+  answer.innerHTML = sum.toString();
+}
+
+
+btn.addEventListener("click", add);
+
+
